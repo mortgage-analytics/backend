3073d68551f2d6f7197d8cc4ab4b282e456ede58
LeonByrne <leonbyrne52@gmail.com>
2024-02-13 12:47:07 +0000

    Added user for testing.

--- Backend/src/main/java/Group28/Backend/BackendApplication.java
+++ Backend/src/main/java/Group28/Backend/BackendApplication.java
@@ -1,13 +1,22 @@
 package Group28.Backend;
 
+import Group28.Backend.controller.DataController;
 import Group28.Backend.controller.LoginController;
+import Group28.Backend.domain.User;
+import Group28.Backend.repository.DataRepository;
+import Group28.Backend.repository.UserRepository;
+import Group28.Backend.service.DataService;
 import Group28.Backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
-public class BackendApplication
+public class BackendApplication implements ApplicationRunner
 {
 	@Autowired
 	LoginController loginController;
@@ -15,8 +24,33 @@ public class BackendApplication
 	@Autowired
 	UserService userService;
 
-	public static void main(String[] args) {
+	@Autowired
+	UserRepository userRepository;
+
+
+	@Autowired
+	DataController dataController;
+
+	@Autowired
+	DataService dataService;
+
+	@Autowired
+	DataRepository dataRepository;
+
+	public static void main(String[] args)
+	{
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception
+	{
+		// Set admin user
+		userService.addUser(new User("byrnel58@tcd.ie", "Test1234_"));
+
+		if(userRepository.existsById("byrnel58@tcd.ie"))
+		{
+			System.out.println("Added");
+		}
+	}
 }
