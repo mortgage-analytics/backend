548808430345ba4c078d21e34abf0e972f158a17
Leon Byrne <leonbyrne52@gmail.com>
2024-03-13 22:32:45 +0000

    Added to git

---
+++ Backend/src/main/java/Group28/Backend/Security/JwtUtil.java
@@ -0,0 +1,43 @@
+package Group28.Backend.Security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET = "THIS=IS=THE=SECRET=FOR=THE=API========================================TODO=MAKE=RANDOM=LATER";
+    private static final long EXPIRATION_TIME = 864_000_000; // 10 days
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+    public static String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET));
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
