d510c8880a82825be65905360201f7483de8f9b1
egentond <egentond@tcd.ie>
2024-03-13 17:44:37 +0000

    Created UserServiceTest class and started writing tests

---
+++ Backend/src/test/java/Group28/Backend/UserServiceTest.java
@@ -0,0 +1,35 @@
+package Group28.Backend;
+import Group28.Backend.repository.*;
+import Group28.Backend.domain.*;
+import Group28.Backend.service.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+    private User testUser;
+
+    @Before
+    public void setup(){
+        testUser = new User("test@something.com", "password");
+    }
+
+    @Test
+    public void addUserTest(){
+        assertEquals(testUser, userService.getUser("test@something.com"));
+
+    }
+
+}
