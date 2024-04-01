c4942f6114d322e1a5e03a52ac739440c908c6aa
LeonByrne <leonbyrne52@gmail.com>
2024-04-02 00:06:35 +0100

    Should now make queries to the "api/data/graphs/byMonth" return far faster.

--- Backend/src/main/java/Group28/Backend/service/ApplicationService.java
+++ Backend/src/main/java/Group28/Backend/service/ApplicationService.java
@@ -1,18 +1,28 @@
 package Group28.Backend.service;
 
 import Group28.Backend.domain.Application;
+import Group28.Backend.domain.MonthlyCount;
 import Group28.Backend.repository.ApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ApplicationService
 {
   @Autowired
   ApplicationRepository applicationRepository;
+
+  private
+  List<MonthlyCount> counts = new ArrayList<>();
+
+  public ApplicationService()
+  {
+    updateCounts();
+  }
 
   public List<Application> getAll()
   {
@@ -86,5 +96,57 @@ public class ApplicationService
     }
 
     return total;
+  }
+
+  public List<MonthlyCount> getCounts()
+  {
+    return counts;
+  }
+
+  // Once per minute
+  @Scheduled(fixedRate = 1000 * 60)
+  private void updateCounts()
+  {
+    List<MonthlyCount> counts = new ArrayList<>();
+
+    // Start and end dates are arbitrary, they just need to encompass all dates
+    Calendar current = Calendar.getInstance();
+    current.set(2000, Calendar.JANUARY, 1);
+
+    Calendar end = Calendar.getInstance();
+    end.set(2025, Calendar.JANUARY, 1);
+
+    // Only get data that is relevant. No need to include any before our first recorded applications
+    boolean firstFound = false;
+    while (current.before(end))
+    {
+      Calendar next = Calendar.getInstance();
+      next.setTime(current.getTime());
+      next.add(Calendar.MONTH, 1);
+
+      int applications = getBetween(current.getTime(), next.getTime()).size();
+      int leads = getBetween(current.getTime(), next.getTime()).size();
+
+      if(firstFound || applications > 0 || leads > 0)
+      {
+        String name = current.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH) + " " + current.get(Calendar.YEAR);
+        counts.add(new MonthlyCount(name, leads, applications));
+
+        firstFound = true;
+      }
+
+      current = next;
+    }
+
+    // Trim the end off. No need to include any after our last recorded applications.
+    for(int i = counts.size() - 1; i >= 0; i--)
+    {
+      if(counts.get(i).getApplications() <= 0 || counts.get(i).getLeads() <= 0)
+      {
+        counts.remove(i);
+      }
+    }
+
+    this.counts = counts;
   }
 }
